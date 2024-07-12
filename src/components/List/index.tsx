95645957188a3f6a87307b630ef63fd3c63fc4c9
Gabrieli Martins <martins.gabrieli.07.almeida@gmail.com>
2024-07-12 16:27:15 -0300

    fist

---
+++ src/components/List/index.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+function List() {
+    const book = [
+        { 
+            materia: "React Js",
+            tempo: "1:00:00"
+        },
+        {
+            materia: "TypeScript",
+            tempo: "0:45:00"
+        },
+        {
+            materia: "Inglês",
+            tempo: "0:50:00"
+        }
+    ];
+    return(
+        <aside>
+            <h2>Estudos do dia</h2>
+            <ul>
+                {book.map((item, index) => (
+                    <li key={index}>
+                        <h3>{item.materia}</h3>
+                        <span>{item.tempo}</span>
+                    </li>
+                ))}
+            </ul>
+        </aside>
+    )
+}
+
+export default List;
