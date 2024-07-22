c5a897f28700aba08bb0727760da2dba7e294033
Gabrieli Martins <martins.gabrieli.07.almeida@gmail.com>
2024-07-22 13:48:41 -0300

    Create props e componentes timer and clock

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -1,10 +1,15 @@
 import Button from "../Button";
+import Clock from "./Clock";
+
+import style from "./timer.module.scss";
 
 export function Timer() {
     return(
-        <div>
-            <p>Escolha um card e inicie o timer</p>
-            <div></div>
+        <div className={style.cronometro}>
+            <p className={style.titulo}>Escolha um card e inicie o timer</p>
+            <div className={style.relogioWrapper}>
+                <Clock />
+            </div>
             <Button text="Iniciar" />
         </div>
     )
